90a7e325e631ce09b03cf008ce36f651d4568563
Rami Chowdhury <rami@socialtables.com>
2015-05-12 16:27:59 -0400

    Cleanups for linter

--- test/index.js
+++ test/index.js
@@ -3,35 +3,35 @@ var findLargestRect = require("../index");
 var testPolygons = require("./polygons.json");
 
 describe("largest-rect-in-poly", function() {
-	it("should find the largest rectangle in a triangle",function(){
+	it("should find the largest rectangle in a triangle", function(){
 		// diffcult to calculate triangles center.
-		var polygon =[[0,0],[0,30],[40,0]];
+		var polygon = [[0, 0], [0, 30], [40, 0]];
 		var rectangle = findLargestRect(polygon);
-		rectangle[0].should.have.property("cx");
-		rectangle[0].should.have.property("cy");
-		rectangle[0].should.have.property("width");
-		rectangle[0].should.have.property("height");
+		should(rectangle[0]).have.property("cx");
+		should(rectangle[0]).have.property("cy");
+		should(rectangle[0]).have.property("width");
+		should(rectangle[0]).have.property("height");
 		var rectArea = rectangle[1];
 		rectArea.should.be.within(240, 300);
 	});
-	it("should find the largest rectangle in a perfect square",function(){
-		var polygon =[[0,0],[0,1000],[1000,1000],[1000,0]];
+	it("should find the largest rectangle in a perfect square", function(){
+		var polygon = [[0, 0], [0, 1000], [1000, 1000], [1000, 0]];
 		var rectangle = findLargestRect(polygon);
-		rectangle[0].should.have.property("cx",500);
-		rectangle[0].should.have.property("cy",500);
+		should(rectangle[0]).have.property("cx", 500);
+		should(rectangle[0]).have.property("cy", 500);
 		var width = rectangle[0].width;
 		var height = rectangle[0].height;
-		width.should.be.within(980, 1020);
-		height.should.be.within(980, 1020);
+		should(width).be.within(980, 1020);
+		should(height).be.within(980, 1020);
 
 	});
-	it("should find the largest rectangle in a perfect rectangle",function(){
-		var polygon =[[0,0],[0,100],[50,100],[50,0]];
+	it("should find the largest rectangle in a perfect rectangle", function(){
+		var polygon = [[0, 0], [0, 100], [50, 100], [50, 0]];
 		var rectangle = findLargestRect(polygon);
-		rectangle[0].should.have.property("cx",25);
-		rectangle[0].should.have.property("cy",50);
-		rectangle[0].should.have.property("width");
-		rectangle[0].should.have.property("height");
+		should(rectangle[0]).have.property("cx", 25);
+		should(rectangle[0]).have.property("cy", 50);
+		should(rectangle[0]).have.property("width");
+		should(rectangle[0]).have.property("height");
 		var width = rectangle[0].width;
 		var height = rectangle[0].height;
 		width.should.be.within(99, 101);
